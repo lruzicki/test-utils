2e6bedf0155b8ea78608fbaec2deb172e4ae68ba
lruzicki <lukasz.ruz@gmail.com>
2023-09-05 09:52:38 +0200

    TECH-790: added draft versioning

--- src/templates/generate-config.py
+++ src/templates/generate-config.py
@@ -4,6 +4,7 @@ import logging
 import pathlib
 import json 
 import itertools
+import subprocess
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -26,17 +27,20 @@ def is_valid(test_harness_path):
       return False 
   return True 
 
+def get_git_tags():
+    tags = subprocess.getoutput("git tag -l")
+    return tags.strip().split("\n")
 
 def app_and_name_from_path(test_harness_path): 
     test_path = str(pathlib.Path(test_harness_path).absolute())
     test_name = str(os.path.basename(test_path))  
     return test_path, test_name
 
-def list_test_executions(example_apps, test_suites): 
+def list_test_executions(example_apps, test_suites, git_tags): 
   # Test are executed for all cancidate applications tested against all test suites
   test_executions = []
-  for element in itertools.product(example_apps, test_suites):
-    app, suite = element 
+  for element in itertools.product(example_apps, test_suites, git_tags):
+    app, suite, tag = element 
     if not (is_valid(app) and is_valid(suite)):
       print(F'---\nTest harness for:\n\tApp: {app}\n\tTest Suite: {suite}"\nwill not be executed.\ntest_entrypoint.sh is missing.\n---')
       continue
@@ -46,7 +50,8 @@ def list_test_executions(example_apps, test_suites):
 
     new_example = {
       'example-app-path': app_path, 'test-suite-name': suite_name, 
-      'example-app-name': app_name, 'test-suite-path': suite_path, 
+      'example-app-name': app_name, 'test-suite-path': suite_path,
+      'bb-version': tag,
       'name': F'{app_name} ({suite_name} test suite)'
     }
     test_executions.append({F"test-example": new_example})
@@ -58,7 +63,8 @@ with open(base_config) as f:
     
     available_examples = [name for name in pathlib.Path(examples_path).iterdir() if os.path.isdir(name)]
     available_test_suites = [name for name in pathlib.Path(test_suites_path).iterdir() if os.path.isdir(name)]
-    circle_config['workflows']['test_everything']['jobs'] = list_test_executions(available_examples, available_test_suites)
+    available_tags = get_git_tags()
+    circle_config['workflows']['test_everything']['jobs'] = list_test_executions(available_examples, available_test_suites, available_tags)
 
     with open(generated_config, "w") as w: 
       yaml.dump(circle_config, w, default_flow_style=False)
